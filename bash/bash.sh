72de4d4ddc7340c92e06722f684f5e078b20517f
Tyler Norlund <tnorlund@icloud.com>
2021-02-12 10:51:25 -0800

    🔨 Refactored bash script

--- bash/bash.sh
+++ bash/bash.sh
@@ -1,20 +1,30 @@
 #!/bin bash
+#
+# Sets up the requirements for the Python Lambda Layer
 
-# Download all the required wheels
-while read p; do
-  curl -O "$p";
-done < require.txt
+if [ ! -d "../python" ]; then
+  # Download all the required wheels
+  while read p; do
+    curl -O "$p" &> /dev/null;
+  done < require.txt
 
-# Unpack all of the wheels
-for i in *.whl; do
-  python3 -m wheel unpack "$i"
-done
-
-# Make the required folder structure
-
-# Move the unpacked packages into the correct location
-for i in */; do 
-  for j in $(ls $i); do
-    mv -f "$i/$j" ../python/lib/python3.8/site-packages
+  # Unpack all of the wheels
+  for i in *.whl; do
+    python3 -m wheel unpack "$i" &> /dev/null;
+    rm $i;
   done
-done
+
+  # Make the required folder structure
+  mkdir ../python;
+  mkdir ../python/lib;
+  mkdir ../python/lib/python3.8
+  mkdir ../python/lib/python3.8/site-packages
+
+  # Move the unpacked packages into the correct location
+  for i in */; do 
+    for j in $(ls $i); do
+      mv -f "$i/$j" ../python/lib/python3.8/site-packages;
+    done
+    rm -rf $i
+  done
+fi
