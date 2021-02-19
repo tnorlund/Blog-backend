ad1ef0cdd985163c3ae516671d91388c4c6abd7b
Tyler Norlund <tnorlund@icloud.com>
2021-02-18 16:09:56 -0800

    ✍️ Finished Docstrings

--- code/lambda/custom_message.js
+++ code/lambda/custom_message.js
@@ -12,7 +12,7 @@ const dynamoDB = new AWS.DynamoDB()
  * The request made from the client.
  * @typedef {Request}
  * @param {string} codeParameter The 4 digits the user uses to verify their
- *   email address.
+ *   email address or phone number.
  */
 
 /**
@@ -24,88 +24,59 @@ const dynamoDB = new AWS.DynamoDB()
  *   triggered.
  * @property {string} region The AWS region the Cognito User Pool is in.
  * @property {Request} request The request sent from the client.
- * @property {Object} response The response sent to Cognito.
+ * @property {Object} response The response sent from this Lambda Function to
+ *   Cognito.
  * @property {string} userName The user name of the user signing up.
  */
 
 /**
- * Sends a custom message to the user's email after signing up.
+ * Sets the custom message during the Cognito User Pool sign up event.
  * @param {Event} event The event triggered by Cognito.
  * @param {Object} context The context of the Lambda Function's event.
  * @param {Function} callback The callback for the Lambda Function.
  */
-exports.handler = ( event, context, callback ) => {
-  console.log( { event } )
-  // Define the URL that you want the user to be directed to after verification
-  // is complete
+exports.handler = async ( event, context, callback ) => {
+  /**
+   * Set the Cognito event when the user signs up.
+   */
   if ( event.triggerSource === `CustomMessage_SignUp` ) {
-    const { codeParameter } = event.request
-    const { region, userName } = event
-    const { clientId } = event.callerContext
-    const redirectUrl = `${process.env.REDIRECTURL}/?username=${userName}`
-    const resourcePrefix = process.env.RESOURCENAME.split( `CustomMessage` )[0]
-
-    // Get the user attributes
-    const { name, email } = event.request.userAttributes
-
-    // Set the table name
-    let tableName = `blogDB`
-    if( process.env.ENV && process.env.ENV !== `NONE` )
-      tableName = tableName + `-` + process.env.ENV
-
-    // Add this user to the DynamoDB.
-    const newUser = new User( { name: name, email: email } )
-
-    // Look through the different regions to see which region the event was
-    // called from.
-    const hyphenRegions = [
-      `us-east-1`, `us-west-1`, `us-west-2`,
-      `ap-southeast-1`, `ap-southeast-2`, `ap-northeast-1`,
-      `eu-west-1`, `sa-east-1`,
-    ]
-    const separator = hyphenRegions.includes( region ) ? `-` : `.`
-
-    // Set the payload for the link to authenticate the user.
-    const payload = Buffer.from(
-      JSON.stringify( { userName, redirectUrl, region, clientId } )
-    ).toString( `base64` )
-
-    // Set the response
-    /**
-     * The url used to verify the email address should be the domain used in the blog.
+    /** 
+     * The 4 digits the client uses to verify the user's email address or phone
+     * number 
      */
-    const url = `https://www.tylernorlund.com/?data=${payload}&code=${codeParameter}`
-    const message = `${process.env.EMAILMESSAGE}. \n ${url}`
-    event.response.smsMessage = message
+    const { codeParameter } = event.request
+
+    /**
+     * The payload used by the client to confirm the user's email or phone number.
+     */
+    const payload = Buffer.from( 
+      JSON.stringify( { userName: event.userName } ) 
+    ).toString( `base64` )
+    
+    /**
+     * The url used to verify the email address.
+     * 
+     * This url is used to confirm the email address of the user signing up.
+     * The url is changed to localhost when under development.
+     */
+    let url
+    if( process.env.ENV && process.env.ENV == 'dev' ) url = `localhost:8000?data=${ 
+      payload }&code=${ codeParameter }`
+    else url = `https://www.tylernorlund.com/?data=${
+      payload }&code=${ codeParameter }`
+
+    /**
+     * Set the event's response.
+     */
+    event.response.smsMessage = `Use the link to verify your phone number. \n ${ url }`
     event.response.emailSubject = process.env.EMAILSUBJECT
-    event.response.emailMessage = message
+    event.response.emailMessage = `Use the link to verify your email address. \n ${ url }`
 
     let blog = new Blog( {} )
 
-    await addUser( {
+    // await Promise( addUser( {
 
-    } )
-    // dynamoDB.updateItem( {
-    //   TableName: tableName,
-    //   Key: blog.key(),
-    //   ConditionExpression: `attribute_exists(PK)`,
-    //   UpdateExpression: `SET #count = #count + :inc`,
-    //   ExpressionAttributeNames: { '#count': `NumberUsers` },
-    //   ExpressionAttributeValues: { ':inc': { 'N': `1` } },
-    //   ReturnValues: `ALL_NEW`
-    // } ).promise().then(
-    //   ( response ) => {
-    //     const requestedBlog = blogFromItem( response.Attributes )
-    //     newUser.userNumber = requestedBlog.numberUsers
-    //     dynamoDB.putItem( {
-    //       TableName: tableName,
-    //       Item: newUser.toItem(),
-    //       ConditionExpression: `attribute_not_exists(PK)`
-    //     } ).promise().then(
-    //       () => callback( null, event )
-    //     ).catch( callback )
-    //   }
-    // ).catch( callback )
+    // } ) )
     callback( null, event )
   } else {
     callback( null, event )
--- code/lambda/post_confirmation.js
+++ code/lambda/post_confirmation.js
@@ -1,8 +1,45 @@
 /* eslint-disable-line */ 
 const aws = require( `aws-sdk` )
 
+/**
+ * The Lambda Function's invocation context.
+ * @typedef {CallerContext}
+ * @param {string} clientId The Cognito User Pool Client ID.
+ */
+
+ /**
+ * The request made from the client.
+ * @typedef {Request}
+ * @param {[Object]} userAttributes The attributes the user created while they
+ *   signed up.
+ */
+
+/**
+ * The Cognito event.
+ * @typedef {Object} Event 
+ * @property {CallerContext} callerContext The Lambda Function's invocation
+ *   context.
+ * @property {string} triggerSource Where the Lambda Function is being
+ *   triggered.
+ * @property {Request} request The request sent from the client.
+ * @property {string} region The AWS region the Cognito User Pool is in.
+ * @property {Object} response The response sent from this Lambda Function to
+ *   Cognito.
+ * @property {string} userName The user name of the user signing up.
+ * @property {string} userPoolId The Cognito User Pool ID.
+ * @property {string} version The Cognito User Pool's version number.
+ */
+
+/**
+ * Adds the user to a Cognito User Pool and DynamoDB after they confirm their
+ *   identity.
+ * @param {Event} event The event triggered by Cognito.
+ * @param {Object} context The context of the Lambda Function's event.
+ * @param {Function} callback The callback for the Lambda Function.
+ */
 exports.handler = async ( event, context, callback ) => {
-  console.log( {event} )
+  console.log( event.request.userAttributes )
+
   const cognitoidentityserviceprovider =
     new aws.CognitoIdentityServiceProvider( { 
       apiVersion: `2016-04-18` 
