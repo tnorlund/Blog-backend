0676ada8c63e4163d2b0f4b5f50e4d5712ea5379
Tyler Norlund <tnorlund@icloud.com>
2021-03-06 15:29:55 -0800

    🔨 Added Kinesis data stream for CloudFront realtime logs

--- terraform/ContentDelivery/main.tf
+++ terraform/ContentDelivery/main.tf
@@ -75,6 +75,7 @@ resource "aws_cloudfront_distribution" "www_distribution" {
     default_ttl            = 86400
     max_ttl                = 31536000
 
+    realtime_log_config_arn = aws_cloudfront_realtime_log_config.analytics.arn
     forwarded_values {
       query_string = false
       cookies {
@@ -82,6 +83,7 @@ resource "aws_cloudfront_distribution" "www_distribution" {
       }
     }
   }
+
 
   /**
    * This sets the aliases of the Cloudfront distribution. Here, it is being
@@ -102,6 +104,260 @@ resource "aws_cloudfront_distribution" "www_distribution" {
     acm_certificate_arn = aws_acm_certificate.certificate.arn
     ssl_support_method  = "sni-only"
   }
+}
+
+/**
+ * These are the permissions for the CloudFront realtime log. It requires access
+ * to the Kineis data stream it uses to store the logs.
+ */
+resource "aws_iam_role" "analytics" {
+  name = "cloudfront-realtime-log"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "kinesis.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_role_policy" "analytics" {
+  name = "cloudfront-realtime-log"
+  role = aws_iam_role.analytics.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Effect": "Allow",
+        "Action": [
+          "kinesis:DescribeStreamSummary",
+          "kinesis:DescribeStream",
+          "kinesis:PutRecord",
+          "kinesis:PutRecords"
+        ],
+        "Resource": "${aws_kinesis_stream.analytics.arn}"
+    }
+  ]
+}
+EOF
+}
+
+/**
+ * This is the realtime logging of the main CloudFront distribution.
+ *
+ * The fields recorded can be found here:
+ *   https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/real-time-logs.html#understand-real-time-log-config-fields
+ */
+resource "aws_cloudfront_realtime_log_config" "analytics" {
+  name          = "analytics"
+  sampling_rate = 100
+  fields        = [
+    "timestamp", 
+    "c-ip",
+    "time-to-first-byte",
+    "sc-status",
+    "sc-bytes",
+    "cs-method",
+    "cs-protocol",
+    "cs-host",
+    "cs-uri-stem",
+    "cs-bytes",
+    "x-edge-location",
+    "x-edge-request-id",
+    "x-host-header",
+    "time-taken",
+    "cs-protocol-version",
+    "c-ip-version",
+    "cs-user-agent",
+    "cs-referer",
+    "cs-cookie",
+    "cs-uri-query",
+    "x-edge-response-result-type",
+    "x-forwarded-for",
+    "ssl-protocol",
+    "ssl-cipher",
+    "x-edge-result-type",
+    "fle-encrypted-fields",
+    "fle-status",
+    "sc-content-type",
+    "sc-content-len",
+    "sc-range-start",
+    "sc-range-end",
+    "c-port",
+    "x-edge-detailed-result-type",
+    "c-country",
+    "cs-accept-encoding",
+    "cs-accept",
+    "cache-behavior-path-pattern",
+    "cs-headers",
+    "cs-header-names",
+    "cs-headers-count"
+  ]
+
+  endpoint {
+    stream_type = "Kinesis"
+
+    kinesis_stream_config {
+      role_arn   = aws_iam_role.analytics.arn
+      stream_arn = aws_kinesis_stream.analytics.arn
+    }
+  }
+
+  depends_on = [aws_iam_role_policy.analytics]
+}
+
+/**
+ * This is the Kinesis data stream used by the main Cloudfront realtime logging.
+ */
+resource "aws_kinesis_stream" "analytics" {
+  name             = "blog-analytics"
+  shard_count      = 1
+  retention_period = 48
+
+  shard_level_metrics = [
+    "IncomingBytes",
+    "OutgoingBytes",
+  ]
+
+  tags = {
+    Environment = "test"
+  }
+}
+
+/**
+ * These are the permissions for the Kinesis Firehose. It requires access to 
+ * the S3 bucket.
+ */
+data "aws_iam_policy_document" "kinesis_firehose" {
+  statement {
+    effect="Allow"
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:PutObject",
+
+      "kinesis:*",
+      # "kinesis:DescribeStreamSummary",
+      # "kinesis:DescribeStream",
+      # "kinesis:PutRecord",
+      # "kinesis:PutRecords",
+
+      # "firehose:DeleteDeliveryStream",
+      # "firehose:PutRecord",
+      # "firehose:PutRecordBatch",
+      # "firehose:UpdateDestination",
+
+
+      # "lambda:InvokeFunction",
+      # "lambda:GetFunctionConfiguration"
+    ]
+    resources = [
+      aws_s3_bucket.bucket.arn,
+      "${aws_s3_bucket.bucket.arn}/*",
+      aws_kinesis_stream.analytics.arn
+    ]
+    sid = "kinesisId"
+  }
+}
+resource "aws_iam_role" "kinesis_firehose" {
+   name = "cloudfront_kinesis_role"
+   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "kinesis.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_role_policy" "kinesis_firehose_stream" {
+  policy = data.aws_iam_policy_document.kinesis_firehose.json
+  role   = aws_iam_role.kinesis_firehose.id
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
+  name        = "tylernorlund-cloudfront-analytics"
+  destination = "extended_s3"
+
+  # kinesis_source_configuration {
+  #   kinesis_stream_arn = aws_kinesis_stream.analytics.arn
+  #   role_arn = aws_iam_role.kinesis_firehose.arn
+  # }
+
+  extended_s3_configuration {
+     cloudwatch_logging_options {
+      log_group_name = "/aws/lambda/tylernorlund_cloudfront_analytics"
+      log_stream_name = "example_stream"
+      enabled = true
+    }
+    role_arn   = aws_iam_role.firehose_role.arn
+    bucket_arn = aws_s3_bucket.bucket.arn
+  }
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "tylernorlund-cloudfront-analytics"
+  acl    = "private"
+}
+
+resource "aws_iam_role" "firehose_role" {
+  name = "firehose_cloudfront"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "firehose.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+data "aws_iam_policy_document" "kinesis_firehose_s3" {
+  statement {
+    effect="Allow"
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:PutObject",
+    ]
+    resources = [
+      aws_s3_bucket.bucket.arn,
+      "${aws_s3_bucket.bucket.arn}/*",
+    ]
+    sid = "kinesisId"
+  }
+}
+resource "aws_iam_role_policy" "kinesis_firehose_stream_s3" {
+  policy = data.aws_iam_policy_document.kinesis_firehose_s3.json
+  role   = aws_iam_role.kinesis_firehose.id
 }
 
 /**
