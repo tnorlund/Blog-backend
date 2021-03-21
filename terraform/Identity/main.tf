108153d82362eba0ce9d1d064ab57bf38884a389
Tyler Norlund <tnorlund@icloud.com>
2021-03-21 11:34:56 -0700

    🔨 Updated GH Action

--- terraform/Identity/main.tf
+++ terraform/Identity/main.tf
@@ -27,11 +27,12 @@ resource "aws_cognito_user_pool" "main" {
   }
 
   password_policy {
-    minimum_length    = "8"
-    require_lowercase = true
-    require_numbers   = true
-    require_symbols   = true
-    require_uppercase = true
+    temporary_password_validity_days = 7
+    minimum_length                   = "8"
+    require_lowercase                = true
+    require_numbers                  = true
+    require_symbols                  = true
+    require_uppercase                = true
   }
 
   lambda_config {
